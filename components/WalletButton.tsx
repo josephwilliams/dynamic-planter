d05e0884330f9dbf7bfd8bd90193cfcdd603bd97
Joseph <josephwilli12@gmail.com>
2024-10-05 01:04:26 +0800

    login button ui

--- components/WalletButton.tsx
+++ components/WalletButton.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import { classNames, DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 
 const WalletButton: React.FC = () => {
+  const { primaryWallet } = useDynamicContext();
+
   return (
     <div className="flex flex-col justify-center items-center mb-2">
-      <div className="mb-[3px] border-[2px] border-[#6e9e6e] border-dotted rounded-md">
+      <div
+        className={classNames(
+          "mb-[3px] border-[2px] border-[#6e9e6e] border-dotted",
+          primaryWallet?.address ? "rounded-md" : "border-none"
+        )}
+      >
         <DynamicWidget />
       </div>
       <div className="text-[12px] text-[#386e39]">
